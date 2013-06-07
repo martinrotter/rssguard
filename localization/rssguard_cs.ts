89f95a4a3412803e89fe31586bf435dfa124c986
Martin Rotter <rotter.martinos@gmail.com>
2013-06-07 18:18:09 +0200

    Added some initial translations, new flags for localizations and some other stuff.

--- localization/rssguard_cs.ts
+++ localization/rssguard_cs.ts
@@ -1,4 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="cs_CZ">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>LANG_NAME</source>
+        <extracomment>Name of language, e.g. English.</extracomment>
+        <translation>Čeština</translation>
+    </message>
+    <message>
+        <source>LANG_ABBREV</source>
+        <extracomment>Abbreviation of language, e.g. en. Use ISO 639-1 code here!</extracomment>
+        <translation>cs</translation>
+    </message>
+    <message>
+        <source>LANG_VERSION</source>
+        <extracomment>Version of your translation, e.g. 1.0.</extracomment>
+        <translation>0.01</translation>
+    </message>
+    <message>
+        <source>LANG_AUTHOR</source>
+        <extracomment>Name of translator - optional.</extracomment>
+        <translation>Martin Rotter</translation>
+    </message>
+    <message>
+        <source>LANG_EMAIL</source>
+        <extracomment>Email of translator - optional.</extracomment>
+        <translation>martinrotter@users.sourceforge.net</translation>
+    </message>
+</context>
 </TS>
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,18 @@
 
 
 int main(int argc, char *argv[]) {
+  //: Name of language, e.g. English.
+  QObject::tr("LANG_NAME");
+  //: Abbreviation of language, e.g. en.
+  //: Use ISO 639-1 code here!
+  QObject::tr("LANG_ABBREV");
+  //: Version of your translation, e.g. 1.0.
+  QObject::tr("LANG_VERSION");
+  //: Name of translator - optional.
+  QObject::tr("LANG_AUTHOR");
+  //: Email of translator - optional.
+  QObject::tr("LANG_EMAIL");
+
   QApplication a(argc, argv);
   QMainWindow window;
   window.show();
