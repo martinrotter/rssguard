60da04aa4d64d6d1afadfaa83a886f61d0375ba4
Martin Rotter <rotter.martinos@gmail.com>
2013-06-26 22:05:09 +0200

    Localization class added.

---
+++ src/core/localization.cpp
@@ -0,0 +1,6 @@
+#include "localization.h"
+
+
+Localization::Localization()
+{
+}
---
+++ src/core/localization.h
@@ -0,0 +1,12 @@
+#ifndef LOCALIZATION_H
+#define LOCALIZATION_H
+
+
+class Localization
+{
+    // TODO: Finish implementation of this class.
+  private:
+    Localization();
+};
+
+#endif // LOCALIZATION_H
