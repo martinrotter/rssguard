5827863910c8f4cfc37b71657fc4cb7b20deb298
Martin Rotter <rotter.martinos@gmail.com>
2014-01-23 18:01:44 +0100

    Work on dark skin.

--- src/gui/plaintoolbutton.cpp
+++ src/gui/plaintoolbutton.cpp
@@ -14,25 +14,8 @@ PlainToolButton::~PlainToolButton() {
 }
 
 void PlainToolButton::paintEvent(QPaintEvent *e) {
+  Q_UNUSED(e)
+
   QPainter p(this);
   icon().paint(&p, QRect(QPoint(0, 0), size()));
-
-  /*
-  if (underMouse()) {
-    QStyleOptionFrameV3 style_option;
-    int frame_shape = QFrame::Sunken & QFrame::Shape_Mask;
-
-    style_option.init(this);
-    style_option.frameShape = QFrame::Shape(int(style_option.frameShape) |
-                                            QFrame::StyledPanel |
-                                            frame_shape);
-    style_option.rect = rect();
-    style_option.lineWidth = 1;
-    style_option.midLineWidth = 0;
-
-    style()->drawControl(QStyle::CE_ShapedFrame,
-                         &style_option, &p,
-                         this);
-  }
-  */
 }
