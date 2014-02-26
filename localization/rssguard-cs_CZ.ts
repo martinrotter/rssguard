b1bbc450edd9157763e6c755c250e67a9180ed23
Martin Rotter <rotter.martinos@gmail.com>
2014-02-26 07:15:17 +0100

    Lang update.

--- localization/rssguard-cs_CZ.ts
+++ localization/rssguard-cs_CZ.ts
@@ -524,23 +524,23 @@
     </message>
     <message>
         <source>Check for updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Zkontrolovat aktualizace</translation>
     </message>
     <message>
         <source>Check if new application updates are available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zkontrolovat, zda nejsou k dispozici aktualizace programu.</translation>
     </message>
     <message>
         <source>Check if new update for the application is available for download.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zkontrolovat, zda nejsou k dispozici aktualizace programu.</translation>
     </message>
     <message>
         <source>Cannot check for updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Nelze zkontrolovat</translation>
     </message>
     <message>
         <source>You cannot check for updates because feed update is ongoing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nelze spustit kontrolu aktualizací, protože běží aktualizace kanálů.</translation>
     </message>
 </context>
 <context>
@@ -1051,7 +1051,7 @@ Autoři této aplikace nenesou žádnou odpovědnost za ztrátu Vašich dat.</tr
     </message>
     <message>
         <source>Keep message selection in the middle of the message list viewport</source>
-        <translation type="unfinished"></translation>
+        <translation>Držet vybranou zprávu uprostřed seznamu zpráv při jejich procházení</translation>
     </message>
     <message>
         <source>You dit not executed any connection test yet.</source>
@@ -1429,27 +1429,27 @@ Autoři této aplikace nenesou žádnou odpovědnost za ztrátu Vašich dat.</tr
     </message>
     <message>
         <source>All metadata fetched successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>Metadata stažena úspěšně.</translation>
     </message>
     <message>
         <source>Feed and icon metadata fetched.</source>
-        <translation type="unfinished"></translation>
+        <translation>Metadata a ikona staženy.</translation>
     </message>
     <message>
         <source>Result: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Výsledek: %1.</translation>
     </message>
     <message>
         <source>Feed or icon metatada not fetched.</source>
-        <translation type="unfinished"></translation>
+        <translation>Metadata nebo ikona nestaženy.</translation>
     </message>
     <message>
         <source>Error: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba: %1.</translation>
     </message>
     <message>
         <source>No metadata fetched.</source>
-        <translation type="unfinished"></translation>
+        <translation>Žádná metadata nestažena.</translation>
     </message>
 </context>
 <context>
@@ -1460,54 +1460,54 @@ Autoři této aplikace nenesou žádnou odpovědnost za ztrátu Vašich dat.</tr
     </message>
     <message>
         <source>Current release</source>
-        <translation type="unfinished"></translation>
+        <translation>Nainstalovaná verze</translation>
     </message>
     <message>
         <source>Available release</source>
-        <translation type="unfinished"></translation>
+        <translation>Dostupná verze</translation>
     </message>
     <message>
         <source>Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Změny</translation>
     </message>
     <message>
         <source>Status</source>
-        <translation type="unfinished"></translation>
+        <translation>Status</translation>
     </message>
     <message>
         <source>unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>neznámá</translation>
     </message>
     <message>
         <source>List with updates was not
 downloaded successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>Seznam aktualizací nebyl úspěšně stažen.</translation>
     </message>
     <message>
         <source>New release available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nová verze aplikace je dostupná.</translation>
     </message>
     <message>
         <source>This is new version which can be
 downloaded and installed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Toto je nová verze, která může být stažena a nainstalována.</translation>
     </message>
     <message>
         <source>This is new version. Upgrade to it manually or via your system package manager.</source>
-        <translation type="unfinished"></translation>
+        <translation>Toto je nová verze. Aktualizujte manuálně nebo přes systémového správce balíčků.</translation>
     </message>
     <message>
         <source>Error: &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba: &apos;%1&apos;.</translation>
     </message>
     <message>
         <source>No new release available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Žádná nová verze k dispozici.</translation>
     </message>
     <message>
         <source>This release is not newer than
 currently installed one.</source>
-        <translation type="unfinished"></translation>
+        <translation>Toto vydání není novější než aktuálně nainstalované.</translation>
     </message>
 </context>
 <context>
@@ -1757,59 +1757,59 @@ Status auto-aktualizace: %5</translation>
     </message>
     <message>
         <source>protocol error</source>
-        <translation type="unfinished"></translation>
+        <translation>chyba protokolu</translation>
     </message>
     <message>
         <source>host not found</source>
-        <translation type="unfinished"></translation>
+        <translation>hostitel nenalezen</translation>
     </message>
     <message>
         <source>connection refused</source>
-        <translation type="unfinished"></translation>
+        <translation>spojení odmítnuto</translation>
     </message>
     <message>
         <source>connection timed out</source>
-        <translation type="unfinished"></translation>
+        <translation>spojení vypršelo</translation>
     </message>
     <message>
         <source>SSL handshake failed</source>
-        <translation type="unfinished"></translation>
+        <translation>SSL handshake selhal</translation>
     </message>
     <message>
         <source>proxy server connection refused</source>
-        <translation type="unfinished"></translation>
+        <translation>spojení k proxy serveru odmítnuto</translation>
     </message>
     <message>
         <source>temporary failure</source>
-        <translation type="unfinished"></translation>
+        <translation>dočasný výpadek</translation>
     </message>
     <message>
         <source>proxy authentication required</source>
-        <translation type="unfinished"></translation>
+        <translation>proxy autentifikace selhala</translation>
     </message>
     <message>
         <source>proxy server not found</source>
-        <translation type="unfinished"></translation>
+        <translation>proxy server nenalezen</translation>
     </message>
     <message>
         <source>success</source>
-        <translation type="unfinished"></translation>
+        <translation>úspěch</translation>
     </message>
     <message>
         <source>uknown content</source>
-        <translation type="unfinished"></translation>
+        <translation>neznámý obsah</translation>
     </message>
     <message>
         <source>unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>neznámá chyba</translation>
     </message>
     <message>
         <source>authentication failed</source>
-        <translation type="unfinished"></translation>
+        <translation>autentifikace selhala</translation>
     </message>
     <message>
         <source>content not found</source>
-        <translation type="unfinished"></translation>
+        <translation>obsah nenalezen</translation>
     </message>
 </context>
 <context>
