f7198031406c651261282af2cff93487278172ed
Martin Rotter <rotter.martinos@gmail.com>
2015-06-26 07:45:19 +0200

    Started working on #12.

---
+++ src/gui/notifications/notification.h
@@ -0,0 +1,40 @@
+// This file is part of RSS Guard.
+//
+// Copyright (C) 2011-2015 by Martin Rotter <rotter.martinos@gmail.com>
+//
+// RSS Guard is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RSS Guard is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RSS Guard. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef NOTIFICATION_H
+#define NOTIFICATION_H
+
+#include <QWidget>
+
+
+class Notification : public QWidget {
+    Q_OBJECT
+
+  public:
+    explicit Notification();
+    virtual ~Notification();
+
+  signals:
+
+  public slots:
+
+  private:
+    void setupWidget();
+
+};
+
+#endif // NOTIFICATION_H
