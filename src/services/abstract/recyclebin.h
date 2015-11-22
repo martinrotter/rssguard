54cf90baa36f0e7edd5454a0660985398c04bf27
Martin Rotter <rotter.martinos@gmail.com>
2015-11-22 19:29:31 +0100

    Save work.

--- src/services/abstract/recyclebin.h
+++ src/services/abstract/recyclebin.h
@@ -30,7 +30,12 @@ class RecycleBin : public RootItem {
 
     QVariant data(int column, int role) const;
 
+    // Empties the bin - removes all messages from it (does not remove
+    // them from DB, just permanently hide them, so that they are not
+    // re-downloaded).
     virtual bool empty() = 0;
+
+    // Performs complete restoration of all messages contained in the bin
     virtual bool restore() = 0;
 };
 
