7d130395332f78060b3460aadca5495ddc52af14
Martin Rotter <rotter.martinos@gmail.com>
2015-12-14 07:22:34 +0100

    Refactor MySQL update script a bit.

--- resources/misc/db_update_mysql_3_4.sql
+++ resources/misc/db_update_mysql_3_4.sql
@@ -19,19 +19,7 @@ CREATE TABLE TtRssAccounts (
 ALTER TABLE Messages
 ADD COLUMN account_id  INTEGER  NOT NULL DEFAULT (1);
 -- !
-ALTER TABLE Feeds
-ADD COLUMN account_id  INTEGER  NOT NULL DEFAULT (1);
--- !
-ALTER TABLE Categories
-ADD COLUMN account_id  INTEGER  NOT NULL DEFAULT (1);
--- !
 ALTER TABLE Messages
-ADD COLUMN custom_id  TEXT;
--- !
-ALTER TABLE Feeds
-ADD COLUMN custom_id  TEXT;
--- !
-ALTER TABLE Categories
 ADD COLUMN custom_id  TEXT;
 -- !
 ALTER TABLE Messages
@@ -39,6 +27,18 @@ DROP FOREIGN KEY feed;
 -- !
 ALTER TABLE Messages
 MODIFY feed TEXT NOT NULL;
+-- !
+ALTER TABLE Messages
+MODIFY author TEXT;
+-- !
+ALTER TABLE Messages
+MODIFY url  TEXT;
+-- !
+ALTER TABLE Feeds
+ADD COLUMN account_id  INTEGER  NOT NULL DEFAULT (1);
+-- !
+ALTER TABLE Feeds
+ADD COLUMN custom_id  TEXT;
 -- !
 ALTER TABLE Feeds
 MODIFY date_created BIGINT;
@@ -53,12 +53,12 @@ ALTER TABLE Feeds
 MODIFY type INTEGER;
 -- !
 ALTER TABLE Categories
+ADD COLUMN account_id  INTEGER  NOT NULL DEFAULT (1);
+-- !
+ALTER TABLE Categories
+ADD COLUMN custom_id  TEXT;
+-- !
+ALTER TABLE Categories
 MODIFY date_created BIGINT;
--- !
-ALTER TABLE Messages
-MODIFY author TEXT;
--- !
-ALTER TABLE Messages
-MODIFY url  TEXT;
 -- !
 UPDATE Information SET inf_value = '4' WHERE inf_key = 'schema_version';
