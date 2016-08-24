a16b81d1f606102fe73921c5b9ca65acb0ab4f75
Martin Rotter <rotter.martinos@gmail.com>
2016-08-24 10:47:51 +0200

    POSIX shell.

--- .travis-before-install.sh
+++ .travis-before-install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if test "$TRAVIS_OS_NAME" = 'osx'; then
   # Mac OS X.
   brew update
-  brew install https://raw.githubusercontent.com/LRFLEW/homebrew-core/981fa2e8f824b068077e7df47f81bdb8d93a8ea1/Formula/qt5.rb
+  brew install qt5
 else
   # Linux.
-  sudo apt-get update
-  sudo apt-get -y install qt57tools qt57base qt57webengine
+  apt-get update
+  apt-get -y install qt57tools qt57base qt57webengine
 fi
