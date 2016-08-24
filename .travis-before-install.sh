09cbb25ad7d5c3c02b54f3d5c1d13e1ac4322669
Martin Rotter <rotter.martinos@gmail.com>
2016-08-24 11:33:06 +0200

    POSIX shell.

--- .travis-before-install.sh
+++ .travis-before-install.sh
@@ -4,10 +4,11 @@ if test "$TRAVIS_OS_NAME" = "osx"; then
   # Mac OS X.
   brew update
   brew install qt5
+  brew link --force qt5
 else
   # Linux.
   sudo add-apt-repository ppa:beineri/opt-qt57-trusty -y
   sudo apt-get update
   sudo apt-get -y install qt57tools qt57base qt57webengine
-  . /opt/qt57/bin/qt57-env.sh
+  bash /opt/qt57/bin/qt57-env.sh
 fi
--- src/core/feeddownloader.cpp
+++ src/core/feeddownloader.cpp
@@ -24,6 +24,7 @@
 #include <QDebug>
 #include <QThreadPool>
 #include <QMutexLocker>
+#include <QString>
 
 
 FeedDownloader::FeedDownloader(QObject *parent)
