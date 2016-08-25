e53b973ae0a81625174f1bc0ccfde4d82b127eb1
Martin Rotter <rotter.martinos@gmail.com>
2016-08-25 22:31:51 +0200

    Share links.

--- resources/scripts/.travis-install-osx.sh
+++ resources/scripts/.travis-install-osx.sh
@@ -4,6 +4,20 @@ mkdir rssguard-build && cd rssguard-build
 qmake ..
 make
 make install
-macdeployqt rssguard.app -dmg
+macdeployqt rssguard-osx.app -dmg
 ls -lha
-curl --upload-file ./*.dmg https://transfer.sh/rssguard.dmg
+
+git clone -q --depth=1 --branch=build-artifacts https://github.com/martinrotter/rssguard.git
+git config --global credential.helper "store --file=github_credentials"
+echo "https://${GH_TOKEN}:@github.com" > github_credentials
+git config --global user.email "rotter.martinos@gmail.com"
+git config --global user.name "martinrotter"
+
+curl --upload-file ./*.dmg https://transfer.sh/rssguard.dmg --silent >> ./build-artifacts/macosx-builds.txt
+
+cat ./build-artifacts/macosx-builds.txt
+
+cd ./rssguard-artifacts
+git add *.*
+git commit -m "New files."
+git push origin build-artifacts
