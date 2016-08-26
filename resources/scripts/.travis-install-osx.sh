d3735d65ee89c46ccbf64f6ed697c981fb967c2e
Martin Rotter <rotter.martinos@gmail.com>
2016-08-26 07:40:21 +0200

    Tweak sharing links.

--- resources/scripts/.travis-install-osx.sh
+++ resources/scripts/.travis-install-osx.sh
@@ -9,6 +9,8 @@ mv rssguard.dmg rssguard-osx.dmg
 
 ls -lha
 
+git config --global user.email "rotter.martinos@gmail.com"
+git config --global user.name "martinrotter"
 git clone -q --depth=1 --branch=build-artifacts https://martinrotter:${GH_TOKEN}@github.com/martinrotter/rssguard.git ./build-artifacts
 curl --upload-file ./rssguard-osx.dmg https://transfer.sh/rssguard-osx.dmg --silent >> ./build-artifacts/macosx-builds.txt
 cat ./build-artifacts/macosx-builds.txt
