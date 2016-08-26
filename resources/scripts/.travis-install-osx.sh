f3b24f2c66e8c6d2a09eda2a2f9e6689b7978cc6
Martin Rotter <rotter.martinos@gmail.com>
2016-08-26 06:31:05 +0200

    Tweak sharing links.

--- resources/scripts/.travis-install-osx.sh
+++ resources/scripts/.travis-install-osx.sh
@@ -4,20 +4,24 @@ mkdir rssguard-build && cd rssguard-build
 qmake ..
 make
 make install
-macdeployqt rssguard-osx.app -dmg
+macdeployqt rssguard.app -dmg
+mv rssguard.dmg rssguard-osx.dmg
+
 ls -lha
 
-git clone -q --depth=1 --branch=build-artifacts https://github.com/martinrotter/rssguard.git
+git clone -q --depth=1 --branch=build-artifacts https://github.com/martinrotter/rssguard.git ./build-artifacts
 git config --global credential.helper "store --file=github_credentials"
 echo "https://${GH_TOKEN}:@github.com" > github_credentials
 git config --global user.email "rotter.martinos@gmail.com"
 git config --global user.name "martinrotter"
 
-curl --upload-file ./*.dmg https://transfer.sh/rssguard.dmg --silent >> ./build-artifacts/macosx-builds.txt
+ls -lha
+
+curl --upload-file ./rssguard-osx.dmg https://transfer.sh/rssguard-osx.dmg --silent >> ./build-artifacts/macosx-builds.txt
 
 cat ./build-artifacts/macosx-builds.txt
 
-cd ./rssguard-artifacts
-git add *.*
+cd ./build-artifacts
+git add *.txt
 git commit -m "New files."
 git push origin build-artifacts
