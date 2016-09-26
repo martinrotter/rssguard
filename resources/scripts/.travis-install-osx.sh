44b9712f1fcddc38fcf707428faef80474c2bf35
Martin Rotter <rotter.martinos@gmail.com>
2016-09-26 08:06:52 +0200

    Fix macos?"

--- resources/scripts/.travis-install-osx.sh
+++ resources/scripts/.travis-install-osx.sh
@@ -13,7 +13,9 @@ git config --global user.email "rotter.martinos@gmail.com"
 git config --global user.name "martinrotter"
 git clone -q --depth=1 https://martinrotter:${GH_TOKEN}@github.com/martinrotter/rssguard.wiki.git ./build-wiki
 
-dmgname=*-osx.dmg
+set -- *.dmg
+dmgname="$1"
+echo $dmgname
 
 curl --upload-file ./$dmgname https://transfer.sh/$dmgname --silent >> ./build-wiki/Mac-OS-X-development-builds.md
 echo >> ./build-wiki/Mac-OS-X-development-builds.md
