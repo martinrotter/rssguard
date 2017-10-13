5b609256446e239ca567a50f9fb80d8a60f18984
Martin Rotter <rotter@praktik.cz>
2017-10-13 14:10:13 +0200

    Let try linuxdeployqt.

--- resources/scripts/.travis-install-linux.sh
+++ resources/scripts/.travis-install-linux.sh
@@ -6,3 +6,9 @@ qmake .. "$qmake_args"
 make lrelease
 make
 make install
+
+ls "./usr/bin"
+curl -o "linuxdeployqt" 'https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage'
+chmod +x "linuxdeployqt"
+linuxdeployqt "./usr/bin/rssguard" -appimage
+ls
