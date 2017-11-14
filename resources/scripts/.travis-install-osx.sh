8f5be1a9e28e81a180f540bda5aae5a463c74b32
Martin Rotter <rotter@praktik.cz>
2017-11-14 10:24:42 +0100

    New dev format - macos.

--- resources/scripts/.travis-install-osx.sh
+++ resources/scripts/.travis-install-osx.sh
@@ -34,7 +34,15 @@ git clone -q --depth=1 https://martinrotter:${GH_TOKEN}@github.com/martinrotter/
 
 url=$(curl --upload-file "./$dmgname" "https://transfer.sh/$dmgnamenospace" --silent)
 
-echo "| $(date +'%m-%d-%Y %T') | [$git_revision](https://github.com/martinrotter/rssguard/commit/$git_revision) | [transfer.sh]($url) | $(echo "$USE_WEBENGINE") |  "$'\r' >> ./build-wiki/Mac-OS-X-development-builds.md
+wikiline="| Mac OS | $(date +'%m-%d-%Y %T') | [$git_revision](https\://github.com/martinrotter/rssguard/commit/$git_revision) | [transfer.sh]($url) | $(echo "$USE_WEBENGINE") |  "
+wikifile="./build-wiki/Development-builds.md"
+wikifilenew="./build-wiki/Development-builds.md.new"
+
+echo "Line to add: $wikiline"
+cat "$wikifile" | sed -e "s@| Mac OS | .\+$USE_WEBENGINE |  @$wikiline@g" > "$wikifilenew"
+
+cat "$wikifilenew"
+#mv "$wikifilenew" "$wikifile"
 
 cd ./build-wiki
 git add *.*
