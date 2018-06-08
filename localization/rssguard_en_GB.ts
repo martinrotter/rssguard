b8ff9cf6d40a818bd85ed437a4a172575c85ee5c
Vinzenz Vietzke <vinz@vinzv.de>
2018-06-08 22:34:06 +0200

    fixed typo in l10n source

--- localization/rssguard_en_GB.ts
+++ localization/rssguard_en_GB.ts
@@ -1098,7 +1098,7 @@ or this functionality is not implemented yet.</source>
         <translation type="unfinished"/>
     </message>
     <message>
-        <source>Predefined settings DO NOT have to be changed from their default values. Change these values only of you are advanced user and you know what you are doing!</source>
+        <source>Predefined settings DO NOT have to be changed from their default values. Change these values only if you are an advanced user and you know what you are doing!</source>
         <translation type="unfinished"/>
     </message>
     <message>
