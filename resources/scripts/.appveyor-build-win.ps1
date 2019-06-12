5226f5395efe6a7368f23985bb46fa8c3b487502
Martin Rotter <rotter@praktik.cz>
2019-06-12 08:20:32 +0200

    fix windows build scripts

--- resources/scripts/.appveyor-build-win.ps1
+++ resources/scripts/.appveyor-build-win.ps1
@@ -3,6 +3,9 @@ $old_pwd=$pwd.Path
 ls
 echo "Qmake args are: '$env:qmake_args'."
 
+# Setup env path with qmake.
+$env:PATH = "$env:QTDIR\bin;" + $env:PATH
+
 mkdir "rssguard-build"
 cd "rssguard-build"
 qmake.exe ..\build.pro "$env:qmake_args"
