ae0ac0d2f2a314909cc4a765a916e38961ea6d29
Martin Rotter <rotter@praktik.cz>
2020-04-28 14:36:00 +0200

    aa

--- resources/scripts/.appveyor-build-win.ps1
+++ resources/scripts/.appveyor-build-win.ps1
@@ -30,13 +30,10 @@ Invoke-WebRequest -Uri $qtbase_url -OutFile $output
 
 $qt_mysql_dir = "./qtbase-everywhere-src-5.14.2/src/plugins/sqldrivers"
 $mysql_d_rev = $mysql_dir.Replace('\', '/')
-$qmake_args_mysql = "MYSQL_INCDIR=$mysql_d_rev/include MYSQL_LIBDIR=$mysql_d_rev/lib"
-
-echo "$qmake_args_mysql"
 
 cd "$qt_mysql_dir"
 
-qmake.exe -- $qmake_args_mysql
+qmake.exe -- MYSQL_INCDIR="$mysql_d_rev/include" MYSQL_LIBDIR="$mysql_d_rev/lib"
 
 nmake.exe sub-mysql
 ls
