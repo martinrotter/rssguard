989fc0acea00d8d7d82de214f05f3be91bb6fbca
Martin Rotter <rotter@praktik.cz>
2020-04-28 11:32:14 +0200

    Fix artifacts.

--- resources/scripts/.appveyor-build-win.ps1
+++ resources/scripts/.appveyor-build-win.ps1
@@ -1,7 +1,9 @@
 # List startup folder.
 $old_pwd=$pwd.Path
 
-ls "C:\OpenSSL-v111-Win64\bin"
+ssl_bin="C:\OpenSSL-v111-Win64\bin"
+
+ls "$ssl_bin"
 ls "C:\Program Files\MySQL\MySQL Server 5.7\lib"
 
 ls "$env:QTDIR"
@@ -24,5 +26,12 @@ nmake.exe install
 cd "app"
 windeployqt.exe --verbose 1 --compiler-runtime --no-translations --release rssguard.exe librssguard.dll
 cd ".."
+
+# Copy OpenSSL.
+Copy-Item -Path "$ssl_bin\libcrypto*.dll" -Destination ".\app\"
+Copy-Item -Path "$ssl_bin\libssl*.dll" -Destination ".\app\"
+
+# Build/copy MySQL Qt plugin.
+
 nmake.exe windows_all
 cd "$old_pwd"
