3d59187937ed3a4f9a03a259bdb53af4ab4ef3e2
Martin Rotter <rotter@praktik.cz>
2020-06-22 10:57:36 +0200

    DB structures for #31.

--- resources/sql/db_init_mysql.sql
+++ resources/sql/db_init_mysql.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS Information (
   inf_value       TEXT        NOT NULL
 );
 -- !
-INSERT INTO Information VALUES (1, 'schema_version', '14');
+INSERT INTO Information VALUES (1, 'schema_version', '15');
 -- !
 CREATE TABLE IF NOT EXISTS Accounts (
   id              INTEGER     PRIMARY KEY,
@@ -128,3 +128,20 @@ CREATE TABLE IF NOT EXISTS Messages (
   
   FOREIGN KEY (account_id) REFERENCES Accounts (id)
 );
+-- !
+CREATE TABLE IF NOT EXISTS MessageFilters (
+  id                  INTEGER     PRIMARY KEY,
+  name                TEXT        NOT NULL CHECK (name != ''),
+  script              TEXT        NOT NULL CHECK (script != '')
+);
+-- !
+CREATE TABLE IF NOT EXISTS MessageFiltersInFeeds (
+  filter                INTEGER     NOT NULL,
+  feed_custom_id        INTEGER     NOT NULL,
+  account_id            INTEGER     NOT NULL,
+  
+  FOREIGN KEY (filter) REFERENCES MessageFilters (id) ON DELETE CASCADE,
+  FOREIGN KEY (account_id) REFERENCES Accounts (id) ON DELETE CASCADE
+);
+-- !
+UPDATE Information SET inf_value = '15' WHERE inf_key = 'schema_version';
