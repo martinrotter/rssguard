c7543f56a362b29be61a7bb254b9f0e275742c4f
Martin Rotter <rotter@praktik.cz>
2020-07-13 10:44:41 +0200

    fix macosx build

--- src/librssguard/miscellaneous/iofactory.cpp
+++ src/librssguard/miscellaneous/iofactory.cpp
@@ -78,7 +78,7 @@ bool IOFactory::startProcessDetached(const QString& program, const QStringList& 
   process.setProgram(program);
   process.setArguments(arguments);
 
-#if !defined(Q_OS_MACOS)
+#if defined(Q_OS_WIN) || defined(Q_CLANG_QDOC)
   process.setNativeArguments(native_arguments);
 #endif
 
