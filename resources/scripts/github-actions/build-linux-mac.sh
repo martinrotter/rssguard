4a241a4b17f5c5ca0b53e66fb44496703f26cc5d
Martin Rotter <rotter@praktik.cz>
2020-12-08 11:21:01 +0100

    Actions

--- resources/scripts/github-actions/build-linux-mac.sh
+++ resources/scripts/github-actions/build-linux-mac.sh
@@ -12,3 +12,14 @@ else
 fi
 
 echo "OS: $os; qmake args: $qmake_args"
+
+# Prepare environment.
+if [ $is_linux = true ]; then
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+  sudo add-apt-repository ppa:beineri/opt-qt-5.15.2-xenial -y
+
+  sudo apt-get update
+  sudo apt-get -y install gcc-7 g++-7 qt515tools qt515base qt515webengine
+else
+  pip3 install aqtinstall
+fi
