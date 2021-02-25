8f2fa52ee9937e92705139c8524d800ab077c599
Martin Rotter <rotter@praktik.cz>
2021-02-25 10:04:07 +0100

    make sure set visible is true on tray icon

--- src/librssguard/gui/systemtrayicon.cpp
+++ src/librssguard/gui/systemtrayicon.cpp
@@ -80,7 +80,6 @@ void SystemTrayIcon::showPrivate() {
 
   // Display the tray icon.
   QSystemTrayIcon::show();
-  setVisible(true);
   emit shown();
 
   qDebugNN << LOGSEC_GUI << "Tray icon displayed.";
