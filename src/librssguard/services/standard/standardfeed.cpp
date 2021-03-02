33fd32672206c161ad32f7c3e4e2e011ab81ee5f
Martin Rotter <rotter@praktik.cz>
2021-03-02 08:39:22 +0100

    fallback for icons

--- src/librssguard/services/standard/standardfeed.cpp
+++ src/librssguard/services/standard/standardfeed.cpp
@@ -451,6 +451,11 @@ StandardFeed* StandardFeed::guessFeed(StandardFeed::SourceType source_type,
     }
   }
 
+  if (source_type == SourceType::Url && icon_possible_locations.isEmpty()) {
+    // We have no source for feed icon, we use the URL of the feed file itself.
+    icon_possible_locations.append({ source, false });
+  }
+
   // Try to obtain icon.
   QIcon icon_data;
 
