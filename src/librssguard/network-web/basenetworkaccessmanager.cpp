3ab3c413015dcd308489c84296a79546190f383b
Martin Rotter <rotter@praktik.cz>
2021-04-01 07:35:03 +0200

    fix headers jesus christ

--- src/librssguard/network-web/basenetworkaccessmanager.cpp
+++ src/librssguard/network-web/basenetworkaccessmanager.cpp
@@ -58,9 +58,7 @@ QNetworkReply* BaseNetworkAccessManager::createRequest(QNetworkAccessManager::Op
   new_request.setAttribute(QNetworkRequest::Attribute::FollowRedirectsAttribute, true);
 #endif
 
-  new_request.setRawHeader(QSL("Cookie").toLocal8Bit(), QSL("JSESSIONID= ").toLocal8Bit());
-
-  // Setup custom user-agent.
+  new_request.setRawHeader(HTTP_HEADERS_COOKIE, QSL("JSESSIONID= ").toLocal8Bit());
   new_request.setRawHeader(HTTP_HEADERS_USER_AGENT, QString(APP_USERAGENT).toLocal8Bit());
 
   auto reply = QNetworkAccessManager::createRequest(op, new_request, outgoingData);
