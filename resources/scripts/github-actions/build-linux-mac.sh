1e000e7494eb58a928ee0efd672d6b461c20af38
Martin Rotter <rotter@praktik.cz>
2021-05-28 07:41:52 +0200

    fix ssl for linux build

--- resources/scripts/github-actions/build-linux-mac.sh
+++ resources/scripts/github-actions/build-linux-mac.sh
@@ -17,18 +17,18 @@ echo "OS: $os; WebEngine: $webengine"
 # Prepare environment.
 if [ $is_linux = true ]; then
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-  sudo add-apt-repository ppa:beineri/opt-qt-5.15.2-xenial -y
+  sudo add-apt-repository ppa:beineri/opt-qt-5.14.2-xenial -y
 
   sudo apt-get update
-  sudo apt-get -y install gcc-9 g++-9 qt515tools qt515base qt515webengine qt515svg qt515multimedia 
+  sudo apt-get -y install gcc-7 g++-7 qt514tools qt514base qt514webengine qt514svg qt514multimedia 
   sudo apt-get -y install openssl libssl-dev libgl1-mesa-dev 
 
   sudo update-alternatives --remove-all gcc 
   sudo update-alternatives --remove-all g++
-  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 50
-  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 50
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 50
   
-  source /opt/qt515/bin/qt515-env.sh
+  source /opt/qt515/bin/qt514-env.sh
 else
   pip3 install aqtinstall
   
