ea3bf01abec967f6e558fa295fbfbef165feb9b9
Martin Rotter <rotter@praktik.cz>
2021-09-20 08:31:55 +0200

    fix adblock icon, messed during refactoring

--- src/librssguard/network-web/adblock/adblockicon.cpp
+++ src/librssguard/network-web/adblock/adblockicon.cpp
@@ -58,5 +58,5 @@ void AdBlockIcon::showMenu(QPoint pos) {
 void AdBlockIcon::setIcon(bool adblock_enabled) {
   QAction::setIcon(adblock_enabled
                    ? qApp->icons()->miscIcon(QSL(ADBLOCK_ICON_ACTIVE))
-                   : qApp->icons()->miscIcon(QSL(ADBLOCK_ICON_ACTIVE)));
+                   : qApp->icons()->miscIcon(QSL(ADBLOCK_ICON_DISABLED)));
 }
