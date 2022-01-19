5319c6c203e6ff679c10b37a955544b00b0b9329
Martin Rotter <rotter@praktik.cz>
2022-01-19 10:57:56 +0100

    tabs to spaces

--- resources/sql/db_init_sqlite.sql
+++ resources/sql/db_init_sqlite.sql
@@ -40,8 +40,8 @@ CREATE TABLE Feeds (
   source          TEXT,
   update_type     INTEGER     NOT NULL CHECK (update_type >= 0),
   update_interval INTEGER     NOT NULL DEFAULT 15 CHECK (update_interval >= 1),
-  is_off          INTEGER		  NOT NULL DEFAULT 0 CHECK (is_off >= 0 AND is_off <= 1),
-  open_articles   INTEGER		  NOT NULL DEFAULT 0 CHECK (open_articles >= 0 AND open_articles <= 1),
+  is_off          INTEGER     NOT NULL DEFAULT 0 CHECK (is_off >= 0 AND is_off <= 1),
+  open_articles   INTEGER     NOT NULL DEFAULT 0 CHECK (open_articles >= 0 AND open_articles <= 1),
   account_id      INTEGER     NOT NULL,
   custom_id       TEXT        NOT NULL CHECK (custom_id != ''), /* Custom ID cannot be empty, it must contain either service-specific ID, or Feeds/id. */
   /* Custom column for (serialized) custom account-specific data. */
