414e0e2fb8737e5a95ca48579b7202626428a085
Martin Rotter <rotter.martinos@gmail.com>
2022-09-15 14:49:57 +0200

    missing files!

---
+++ resources/sql/db_update_mysql_2_3.sql
@@ -0,0 +1,7 @@
+USE ##;
+-- !
+SET FOREIGN_KEY_CHECKS = 0;
+-- !
+!! db_update_sqlite_2_3.sql
+-- !
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ resources/sql/db_update_sqlite_2_3.sql
@@ -0,0 +1,2 @@
+UPDATE Feeds
+SET update_interval = update_interval * 60;
