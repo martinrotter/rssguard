7197c9d0c65bbdda3cc432add82ab5e511f58ec4
Martin Rotter <rotter.martinos@gmail.com>
2023-04-17 06:21:57 +0200

    minor adjustments for persistent article viewer

--- src/librssguard/gui/itemdetails.cpp
+++ src/librssguard/gui/itemdetails.cpp
@@ -13,7 +13,7 @@ ItemDetails::~ItemDetails() {}
 
 void ItemDetails::loadItemDetails(RootItem* item) {
   if (item == nullptr) {
-    m_ui.m_lblIcon->setPixmap(QPixmap(APP_ICON_PATH).scaled(128, 128));
+    m_ui.m_lblIcon->setPixmap(QPixmap(APP_ICON_PATH).scaled(16, 16));
     m_ui.m_lblInfo->setText(QSL("<b>%1</b>").arg(QSL(APP_LONG_NAME)));
   }
   else {
@@ -29,7 +29,7 @@ void ItemDetails::loadItemDetails(RootItem* item) {
       tool_tip += QL1S("<br/><br/>") + extra_tooltip.replace(QSL("\n"), QSL("<br/>"));
     }
 
-    m_ui.m_lblIcon->setPixmap(item->fullIcon().pixmap({128, 128}));
+    m_ui.m_lblIcon->setPixmap(item->fullIcon().pixmap({16, 16}));
     m_ui.m_lblInfo->setText(tool_tip);
   }
 }
