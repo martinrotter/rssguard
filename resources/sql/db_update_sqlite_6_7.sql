ad94ce2f0e3e590ef920f661e1578933daf59d26
Martin Rotter <rotter.martinos@gmail.com>
2023-08-06 07:52:09 +0200

    fix db v7 schema migration sql code

--- resources/sql/db_update_sqlite_6_7.sql
+++ resources/sql/db_update_sqlite_6_7.sql
@@ -2,7 +2,7 @@ CREATE TABLE Probes (
   id                  $$,
   name                TEXT        NOT NULL CHECK (name != ''),
   color               VARCHAR(7)  NOT NULL CHECK (color != ''),
-  fltr              TEXT        NOT NULL CHECK (filter != ''), /* Regular expression. */
+  fltr                TEXT        NOT NULL CHECK (fltr != ''), /* Regular expression. */
   account_id          INTEGER     NOT NULL,
   
   FOREIGN KEY (account_id) REFERENCES Accounts (id) ON DELETE CASCADE
