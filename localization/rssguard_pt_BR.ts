f25e293d88628db6398281cc52d885419694f2c4
martinrotter <martinrotter@users.noreply.github.com>
2023-08-24 14:01:36 +0200

    Portuguese, Brazilian translation for rssguard_en.ts [skip ci]

--- localization/rssguard_pt_BR.ts
+++ localization/rssguard_pt_BR.ts
@@ -281,6 +281,16 @@ versão, clicando nesta notificação popup.</translation>
       <translation>Sem autenticação</translation>
     </message>
     <message>
+      <location filename="../src/librssguard/services/abstract/gui/authenticationdetails.cpp" line="19"/>
+      <source>HTTP Basic</source>
+      <translation type="unfinished">HTTP Basic</translation>
+    </message>
+    <message>
+      <location filename="../src/librssguard/services/abstract/gui/authenticationdetails.cpp" line="22"/>
+      <source>Token</source>
+      <translation type="unfinished">Token</translation>
+    </message>
+    <message>
       <location filename="../src/librssguard/services/abstract/gui/authenticationdetails.cpp" line="53"/>
       <source>Username/token is ok or it is not needed.</source>
       <translation>Nome de usuário/token está OK ou não é necessário.</translation>
