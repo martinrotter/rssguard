f84973dfcdb0efc4f5bf1a0bfc0bee647e9f8ca8
Martin Rotter <rotter.martinos@gmail.com>
2023-08-24 06:41:34 +0200

    langs

--- localization/rssguard_en.ts
+++ localization/rssguard_en.ts
@@ -276,6 +276,16 @@ version by clicking this popup notification.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/librssguard/services/abstract/gui/authenticationdetails.cpp" line="19"/>
+        <source>HTTP Basic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/librssguard/services/abstract/gui/authenticationdetails.cpp" line="22"/>
+        <source>Token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../src/librssguard/services/abstract/gui/authenticationdetails.cpp" line="53"/>
         <source>Username/token is ok or it is not needed.</source>
         <translation type="unfinished"></translation>
