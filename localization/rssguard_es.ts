ad70999b54319ef8a9acd2cfcad3822519830aef
martinrotter <martinrotter@users.noreply.github.com>
2023-10-03 16:15:31 +0200

    Spanish translation for rssguard_en.ts [skip ci]

--- localization/rssguard_es.ts
+++ localization/rssguard_es.ts
@@ -242,8 +242,13 @@ versión pulsando en esta notificación emergente.</translation>
       <source>Open article in web browser</source>
       <translation>Abrir artículo en el navegador web</translation>
     </message>
+    <message>
+      <location filename="../src/librssguard/gui/notifications/articlelistnotification.ui" line="141"/>
+      <source>Mark all articles as read</source>
+      <translation type="unfinished">Mark all articles as read</translation>
+    </message>
     <message numerus="yes">
-      <location filename="../src/librssguard/gui/notifications/articlelistnotification.cpp" line="65"/>
+      <location filename="../src/librssguard/gui/notifications/articlelistnotification.cpp" line="72"/>
       <source>%n feeds fetched</source>
       <translation>
         <numerusform>%n fuentes obtenidas</numerusform>
@@ -348,7 +353,7 @@ versión pulsando en esta notificación emergente.</translation>
   <context>
     <name>BaseToastNotification</name>
     <message>
-      <location filename="../src/librssguard/gui/notifications/basetoastnotification.cpp" line="37"/>
+      <location filename="../src/librssguard/gui/notifications/basetoastnotification.cpp" line="41"/>
       <source>Close this notification</source>
       <translation>Cerrar esta notificación</translation>
     </message>
@@ -5255,7 +5260,7 @@ Feedly is a secure space where you can privately organize and research the topic
 Feedly es un espacio seguro donde puede organizar e investigar de forma privada los temas y tendencias que le interesan.</translation>
     </message>
     <message>
-      <location filename="../src/librssguard/database/databasequeries.cpp" line="2650"/>
+      <location filename="../src/librssguard/database/databasequeries.cpp" line="2657"/>
       <source>Cannot insert article filter, because current database cannot return last inserted row ID.</source>
       <translation>Imposible insertar el filtro de artículo porque la base de datos actual no puede devolver el ID de la última fila insertada.</translation>
     </message>
