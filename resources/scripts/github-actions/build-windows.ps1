a5ab678848ae102dad7b018920b6b88dfdc694cc
Martin Rotter <rotter.martinos@gmail.com>
2025-02-07 10:20:46 +0100

    bump qt/mariadb versions

--- resources/scripts/github-actions/build-windows.ps1
+++ resources/scripts/github-actions/build-windows.ps1
@@ -103,10 +103,10 @@ pip3 install -U pip
 pip3 install -I aqtinstall
 
 if ($is_qt_6) {
-  aqt -c 'aqt\settings.ini' install-qt -O "$qt_path" windows desktop $qt_version win64_msvc2019_64 -m qtwebengine qtimageformats qtmultimedia qt5compat qtwebchannel qtpositioning
+  aqt -c 'aqt\settings.ini' install-qt -O "$qt_path" windows desktop $qt_version win64_msvc2022_64 -m qtwebengine qtimageformats qtmultimedia qt5compat qtwebchannel qtpositioning
 }
 else {
-  aqt -c 'aqt\settings.ini' install-qt -O "$qt_path" windows desktop $qt_version win64_msvc2019_64 -m qtwebengine
+  aqt -c 'aqt\settings.ini' install-qt -O "$qt_path" windows desktop $qt_version win64_msvc2022_64 -m qtwebengine
 }
 
 aqt -c 'aqt\settings.ini' install-src -O "$qt_path" windows desktop $qt_version --archives qtbase
