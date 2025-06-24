f9dff769801a2204cc775a96f1698a5edfcdef12
Martin Rotter <rotter.martinos@gmail.com>
2025-06-24 07:44:32 +0200

    separate master CSS into file, some path fixing etc

--- src/librssguard/gui/webviewers/qlitehtml/container_qpainter.cpp
+++ src/librssguard/gui/webviewers/qlitehtml/container_qpainter.cpp
@@ -3,7 +3,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include "container_qpainter.h"
+#include "gui/webviewers/qlitehtml/container_qpainter.h"
 
 #if QT_CONFIG(clipboard)
 #include <QClipboard>
