5a3a06cd1ebfdf945b00029a01cc45ba91b8c145
Martin Rotter <rotter.martinos@gmail.com>
2025-10-02 20:41:59 +0200

    workflow with container for ubuntu

---
+++ resources/scripts/github-actions/prepare-linux.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	gtk3             \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt6ct            \
+	qt6-wayland      \
+	rssguard         \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-mesa --prefer-nano opus-mini
+
+pacman -Q rssguard | awk '{print $2; exit}' > ~/version
