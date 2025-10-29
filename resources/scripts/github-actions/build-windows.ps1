2ccc14e20e24ce93ff84818610a3ef30ee590c40
Martin Rotter <rotter.martinos@gmail.com>
2025-10-29 06:47:08 +0100

    fetch keyring for linux build

--- resources/scripts/github-actions/build-windows.ps1
+++ resources/scripts/github-actions/build-windows.ps1
@@ -53,7 +53,7 @@ if ($use_qt5 -eq "ON") {
   $with_qt6 = "OFF"
 }
 else {
-  $qt_version = "6.9.3"
+  $qt_version = "6.10.0"
   $qt_arch_base = "msvc2022_64"
 
   $use_libmpv = "ON"
--- resources/scripts/github-actions/prepare-linux.sh
+++ resources/scripts/github-actions/prepare-linux.sh
@@ -5,50 +5,51 @@ set -eux
 ARCH="$(uname -m)"
 EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
-pacman -Syu --noconfirm \
-    appstream        \
-    base-devel       \
-    cmake            \
-    curl             \
-    gcc-libs         \
-    git              \
-    glibc            \
-    gtk3             \
-    libheif          \
-    libglvnd         \
-    libxcb           \
-    libxcursor       \
-    libxi            \
-    libxkbcommon     \
-    libxkbcommon-x11 \
-    libxrandr        \
-    libxtst          \
-    make             \
-    mariadb-clients  \
-    mpv              \
-    ninja            \
-    pipewire-audio   \
-    pulseaudio       \
-    pulseaudio-alsa  \
-    qt5-base         \
-    qt5-declarative  \
-    qt5-imageformats \
-    qt5-multimedia   \
-    qt5-tools        \
-    qt5-wayland      \
-    qt5-svg          \
-    qt6-5compat      \
-    qt6-base         \
-    qt6-declarative  \
-    qt6-imageformats \
-    qt6-multimedia   \
-    qt6-tools        \
-    qt6-wayland      \
-    qt6-svg          \
-    qt6ct            \
-    sqlite           \
-    wget             \
-    xorg-server-xvfb \
+pacman -Syu --noconfirm  \
+    appstream            \
+    archlinux-keyring    \
+    base-devel           \
+    cmake                \
+    curl                 \
+    gcc-libs             \
+    git                  \
+    glibc                \
+    gtk3                 \
+    libheif              \
+    libglvnd             \
+    libxcb               \
+    libxcursor           \
+    libxi                \
+    libxkbcommon         \
+    libxkbcommon-x11     \
+    libxrandr            \
+    libxtst              \
+    make                 \
+    mariadb-clients      \
+    mpv                  \
+    ninja                \
+    pipewire-audio       \
+    pulseaudio           \
+    pulseaudio-alsa      \
+    qt5-base             \
+    qt5-declarative      \
+    qt5-imageformats     \
+    qt5-multimedia       \
+    qt5-tools            \
+    qt5-wayland          \
+    qt5-svg              \
+    qt6-5compat          \
+    qt6-base             \
+    qt6-declarative      \
+    qt6-imageformats     \
+    qt6-multimedia       \
+    qt6-tools            \
+    qt6-wayland          \
+    qt6-svg              \
+    qt6ct                \
+    sqlite               \
+    wget                 \
+    xorg-server-xvfb     \
     zsync
 
 echo "Installing debloated packages..."
