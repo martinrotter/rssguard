25ef72595f0e10dd794a59283b6daf5da788e0b4
Martin Rotter <rotter.martinos@gmail.com>
2025-11-13 10:04:05 +0100

    regen langs

--- src/librssguard-standard/src/gui/standardfeedexpdetails.cpp
+++ src/librssguard-standard/src/gui/standardfeedexpdetails.cpp
@@ -20,6 +20,11 @@
 StandardFeedExpDetails::StandardFeedExpDetails(QWidget* parent) : QWidget(parent) {
   m_ui.setupUi(this);
 
+  m_ui.m_helpFetchFullArticles
+    ->setHelpText(tr("Turning this setting ON will fetch full source articles for this feed. Note that this will slow "
+                     "down feed fetching and also can make your database considerably bigger."),
+                  true);
+
   m_ui.m_helpDontUseRawXml->setHelpText(tr("Turning this setting ON might bring considerable performance boost when "
                                            "fetching this feed, but only in some very specific conditions.\n\n"
                                            "This setting is useful when raw XML parsing of the feed is very slow, this "
@@ -29,5 +34,5 @@ StandardFeedExpDetails::StandardFeedExpDetails(QWidget* parent) : QWidget(parent
   m_ui.m_helpFetchComments->setHelpText(tr("This enables fetching of embedded article comments. Note that if the feed "
                                            "has many articles and each of them has comments, then the whole feed "
                                            "fetching can be much much slower with this option enabled."),
-                                        false);
+                                        true);
 }
