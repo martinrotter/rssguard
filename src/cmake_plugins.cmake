8a5ce05b72ec7f29a9b12af8bacb9193db90fa0f
Martin Rotter <rotter.martinos@gmail.com>
2025-11-18 07:52:02 +0100

    cleanup plugin cmake a bit

--- src/cmake_plugins.cmake
+++ src/cmake_plugins.cmake
@@ -12,8 +12,6 @@ function(prepare_rssguard_plugin plugin_target_name)
     list(APPEND SOURCES "${CMAKE_BINARY_DIR}/rssguard.rc")
   endif()
 
-  include_directories(${CMAKE_SOURCE_DIR}/src/librssguard/3rd-party/qtlinq/include)
-
   add_library(${PLUGIN_TARGET} SHARED ${SOURCES} ${QM_FILES})
 
   target_link_libraries(${plugin_target_name} PUBLIC
@@ -28,6 +26,7 @@ function(prepare_rssguard_plugin plugin_target_name)
   target_include_directories(${plugin_target_name}
     PUBLIC
     ${LIBRSSGUARD_SOURCE_PATH}
+    ${LIBRSSGUARD_SOURCE_PATH}/librssguard/3rd-party/qtlinq/include
   )
 
   if((WIN32 AND NOT BUILD_MSYS2) OR OS2)
