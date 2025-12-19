27234c2b2965e6cf1b02c631ef4aeca603939213
martinrotter <martinrotter@users.noreply.github.com>
2025-12-19 15:42:05 +0100

    Chinese Simplified translation for rssguard_en.ts [skip ci]

--- localization/rssguard_zh_CN.ts
+++ localization/rssguard_zh_CN.ts
@@ -469,7 +469,7 @@ Number of disabled feeds: %3</source>
       <location filename="../src/librssguard/database/databasewriter.cpp" line="51"/>
       <location filename="../src/librssguard/database/databasewriter.cpp" line="84"/>
       <source>database writer is stopping</source>
-      <translation type="unfinished">database writer is stopping</translation>
+      <translation>数据库写入器正在停止</translation>
     </message>
   </context>
   <context>
@@ -1156,17 +1156,17 @@ Item custom ID: %5</source>
     <message>
       <location filename="../src/librssguard/gui/feedsview.cpp" line="304"/>
       <source>Enable or disable feeds</source>
-      <translation type="unfinished">Enable or disable feeds</translation>
+      <translation>启用或禁用订阅源</translation>
     </message>
     <message>
       <location filename="../src/librssguard/gui/feedsview.cpp" line="305"/>
       <source>You selected multiple feeds to enable/disable them.</source>
-      <translation type="unfinished">You selected multiple feeds to enable/disable them.</translation>
+      <translation>您选择了多个源来启用/禁用它们。</translation>
     </message>
     <message>
       <location filename="../src/librssguard/gui/feedsview.cpp" line="306"/>
       <source>Do you really want to enable or disable selected feeds?</source>
-      <translation type="unfinished">Do you really want to enable or disable selected feeds?</translation>
+      <translation>您真的想要启用或禁用选定的源吗？</translation>
     </message>
     <message>
       <location filename="../src/librssguard/gui/feedsview.cpp" line="381"/>
@@ -5626,22 +5626,22 @@ Token 过期时间：%2</translation>
       <location filename="../src/librssguard/gui/messagesview.cpp" line="542"/>
       <location filename="../src/librssguard/gui/messagesview.cpp" line="543"/>
       <source>%n selected articles(s)</source>
-      <translation type="unfinished">
-        <numerusform>%n selected articles(s)</numerusform>
+      <translation>
+        <numerusform>选中了 %n 篇文章</numerusform>
       </translation>
     </message>
     <message>
       <location filename="../src/librssguard/gui/messagesview.cpp" line="676"/>
       <location filename="../src/librssguard/gui/messagesview.cpp" line="985"/>
       <source>Filtering changed</source>
-      <translation type="unfinished">Filtering changed</translation>
+      <translation>筛选已更改</translation>
     </message>
     <message numerus="yes">
       <location filename="../src/librssguard/gui/messagesview.cpp" line="677"/>
       <location filename="../src/librssguard/gui/messagesview.cpp" line="986"/>
       <source>%n article(s) left after filtering change</source>
-      <translation type="unfinished">
-        <numerusform>%n article(s) left after filtering change</numerusform>
+      <translation>
+        <numerusform>筛选更改后剩下 %n 篇文章</numerusform>
       </translation>
     </message>
     <message>
@@ -5680,7 +5680,7 @@ Token 过期时间：%2</translation>
     <message>
       <location filename="../src/librssguard/gui/messagebox.cpp" line="107"/>
       <source>Do not show again</source>
-      <translation type="unfinished">Do not show again</translation>
+      <translation>不再显示</translation>
     </message>
   </context>
   <context>
@@ -6490,9 +6490,9 @@ List of supported readers:</source>
       <source>%1
 line: %2
 stack: %3</source>
-      <translation type="unfinished">%1
-line: %2
-stack: %3</translation>
+      <translation>%1
+行： %2
+堆栈：%3</translation>
     </message>
   </context>
   <context>
