5927ff9c2ce998786af03d9f6f0737c6d2ca120e
Martin Rotter <rotter.martinos@gmail.com>
2026-03-04 10:53:47 +0100

    fix #2081

--- src/rssguard/main.cpp
+++ src/rssguard/main.cpp
@@ -74,11 +74,6 @@ int main(int argc, char* argv[]) {
   // NOTE: https://bugreports.qt.io/browse/QTBUG-117612
   qputenv("QT_DISABLE_AUDIO_PREPARE", "1");
 
-#if defined(Q_OS_WIN) && QT_VERSION_MAJOR > 5
-  // NOTE: Turn off dark mode detection on Windows.
-  qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
-#endif
-
   // High DPI stuff.
 #if QT_VERSION >= 0x050E00 // Qt >= 5.14.0
   auto high_dpi_existing_env = qgetenv("QT_ENABLE_HIGHDPI_SCALING");
